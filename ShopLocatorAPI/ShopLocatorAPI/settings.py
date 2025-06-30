098343415b69cd349a3b817efa7dc4f88c94b000
Buddy <Pro100biv@mail.ru>
2025-06-30 22:14:17 +0400

    add: postgresql activate and added app shop to settings.py

--- ShopLocatorAPI/ShopLocatorAPI/settings.py
+++ ShopLocatorAPI/ShopLocatorAPI/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'shops.apps.ShopConfig',
 ]
 
 MIDDLEWARE = [
@@ -74,8 +75,12 @@ WSGI_APPLICATION = 'ShopLocatorAPI.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'shop_locator',
+        'USER': 'shop_user',
+        'PASSWORD': 'admin',
+        'HOST': 'localhost',
+        'PORT': 5432,
     }
 }
 
