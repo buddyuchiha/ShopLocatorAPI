7886d3460cb8d5b9e054d8618f8810df790c2e9a
Buddy <Pro100biv@mail.ru>
2025-07-01 15:05:43 +0400

    Removed cached files and updated .gitignore

--- ShopLocatorAPI/shops/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path, re_path, register_converter
-from . import views
-
-urlpatterns = [
-    path('', views.MyView.as_view(), name='home'),
-    path('city/', views.GetCity.as_view(), name='cities'),
-    path('city/street/', views.GetCityStreets.as_view(), name='streets'),
-    path('shop/', views.PostShop.as_view(), name='shops'),
-]
