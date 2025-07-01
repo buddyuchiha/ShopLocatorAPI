01907d2176444f3b8765ab1af49ef0a27177b7ea
Buddy <Pro100biv@mail.ru>
2025-07-01 15:02:13 +0400

    added: urls and the beginning structure of the CBV

--- ShopLocatorAPI/shops/views.py
+++ ShopLocatorAPI/shops/views.py
@@ -1,3 +1,36 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.views.generic import View, ListView
 
 # Create your views here.
+
+class MyView(View):
+    def get(self, request, *args, **kwargs):
+        return HttpResponse('GET request')
+    
+    def post(self, request, *args, **kwargs):
+        return HttpResponse('POST request')
+    
+
+class GetCity(View):
+    def get(self, request, *args, **kwargs):
+        return HttpResponse('GET GetCity request')
+    
+    def post(self, request, *args, **kwargs):
+        return HttpResponse('POST GetCity request')
+    
+class GetCityStreets(View):
+    def get(self, request, *args, **kwargs):
+        return HttpResponse('GET GetCityStreets request')
+    
+    def post(self, request, *args, **kwargs):
+        return HttpResponse('POST GetCityStreets request')
+    
+
+class PostShop(View):
+    def get(self, request, *args, **kwargs):
+        return HttpResponse('GET PostShop request')
+    
+    def post(self, request, *args, **kwargs):
+        return HttpResponse('POST PostShop request')
+    
